c56c97b9f2ff245f12510bcbfee43a429452ae55
diegolopez03 <diego232.lopez@gmail.com>
2020-10-15 15:11:27 -0600

    Creación del archivo index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+// Importar módulos necesarios 
+//Express
+const express = require("express");
+
+
+
+
+
+
+
+
+// Crear un servidor express
+const app = express();
+
+
+
+
+
+
+
+
+
+
+
+
+// Inicializar el servidor en un puerto
+app.listen(5000, () => {
+    console.log("Servidor ejecutándose en el puerto 5000");
+  });
