2986f4a367b1dbbf204a67cbe5ee4b13f46c1b78
diegolopez03 <diego232.lopez@gmail.com>
2020-10-15 21:43:17 -0600

    Creación de función para el cálculo

---
+++ calculoInteres.js
@@ -0,0 +1,29 @@
+exports.calculoInteres = (monto, periodo, interes) => {
+
+    let tasaInteresDecimal = interes / 100;
+    let interesS = 0; 
+    let saldoFinal = 0;
+    let filas = [];
+
+    periodo = Math.round(periodo * 12);
+    
+    saldoFinal = monto + interesS; 
+     
+    
+
+
+    for (let i = 1; i <= periodo; i++) {
+        saldoFinal = monto * ((1 + tasaInteresDecimal) ** i); 
+        interesS = saldoFinal * tasaInteresDecimal; 
+        interesS.toFixed(2);
+        saldoFinal = saldoFinal + interesS;
+        saldoFinal.toFixed(2);
+        filas.push({
+            i,
+            monto,
+            interesS,
+            saldoFinal
+        });
+    }
+    return filas;
+}
